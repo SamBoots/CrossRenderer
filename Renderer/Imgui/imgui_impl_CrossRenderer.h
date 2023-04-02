a5df9d4afa7b3c3df903c169c7b857400bc52011
Sam <samboots123@gmail.com>
2023-04-03 01:42:16 +0200

    prepare for custom imgui func

---
+++ Renderer/Imgui/imgui_impl_CrossRenderer.h
@@ -0,0 +1,18 @@
+// dear imgui: Renderer Backend for CrossRenderer
+
+#include "imgui.h"      // IMGUI_IMPL_API
+#include "RenderBackendCommon.h"
+
+struct ImGui_ImplCross_InitInfo
+{
+
+};
+
+// Called by user code
+IMGUI_IMPL_API bool ImGui_ImplCross_Init(const ImGui_ImplCross_InitInfo& a_Info);
+IMGUI_IMPL_API void ImGui_ImplCross_Shutdown();
+IMGUI_IMPL_API void ImGui_ImplCross_NewFrame();
+IMGUI_IMPL_API void ImGui_ImplCross_RenderDrawData(const ImDrawData& a_DrawData, const BB::RecordingCommandListHandle a_CommandList, const BB::PipelineHandle a_Pipeline = nullptr);
+IMGUI_IMPL_API bool ImGui_ImplCross_CreateFontsTexture(const BB::RecordingCommandListHandle a_CommandList);
+IMGUI_IMPL_API void ImGui_ImplCross_DestroyFontUploadObjects();
+IMGUI_IMPL_API void ImGui_ImplCross_SetMinImageCount(const uint32_t min_image_count); // To override MinImageCount after initialization (e.g. if swap chain is recreated)
