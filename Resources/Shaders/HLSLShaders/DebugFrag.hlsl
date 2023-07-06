e953654feaecba24d3026086515f274efa27d7bf
Sam <samboots123@gmail.com>
2023-07-06 18:01:19 +0200

    ambient light is busted

--- Resources/Shaders/HLSLShaders/DebugFrag.hlsl
+++ Resources/Shaders/HLSLShaders/DebugFrag.hlsl
@@ -27,8 +27,8 @@ struct Light
 };
 
 //Maybe add in common if I find a way to combine them.
-_BBBIND(0, 1) StructuredBuffer<BaseFrameInfo> baseFrameInfo : register(t0, space0);
-_BBBIND(3, 1) StructuredBuffer<Light> lights : register(t3, space0);
+_BBBIND(0, 1) ByteAddressBuffer baseFrameInfo : register(t0, space0);
+_BBBIND(3, 1) ByteAddressBuffer lights : register(t3, space0);
 
 _BBBIND(0, 0) SamplerState samplerColor : register(s0, space0);
 _BBBIND(4, 1) Texture2D text : register(t4, space0);
@@ -45,22 +45,25 @@ struct VSoutput
 
 float4 main(VSoutput input) : SV_Target
 {
+    const BaseFrameInfo t_FrameInfo = baseFrameInfo.Load<BaseFrameInfo>(0);
+    
     float4 t_TextureColor = text.Sample(samplerColor, input.uv);
     float4 t_Color = t_TextureColor * float4(input.color.xyz, 1.0f);
     
     float4 t_Diffuse;
     //Apply lights
-    for (int i = 0; i < baseFrameInfo[0].lightCount; i++)
+    for (int i = 0; i < t_FrameInfo.lightCount; i++)
     {
+        Light t_Light = lights.Load<Light>(sizeof(Light) * i);
         float3 t_Normal = normalize(input.normal);
-        float3 t_Dir = normalize(lights[i].pos - input.fragPos);
+        float3 t_Dir = normalize(t_Light.pos - input.fragPos);
 
         float t_Diff = max(dot(t_Normal, t_Dir), 0.0f);
-        t_Diffuse = mul(t_Diff, lights[i].color);
+        t_Diffuse = mul(t_Diff, t_Light.color);
     }
 
     //Apply the Light colors;
-    float4 t_Ambient = float4(mul(baseFrameInfo[0].ambientLight, baseFrameInfo[0].ambientStrength), 1.0f);
+    float4 t_Ambient = float4(mul(t_FrameInfo.ambientLight, t_FrameInfo.ambientStrength), 1.0f);
     
     float4 t_Result = (t_Ambient + t_Diffuse) * t_Color;
     return t_Result;
